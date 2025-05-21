d2ef34748a74c9e02e9aac8ce86e77e575e8acb9
Lorenzo Latinne <lorenzo.latinne@student.pxl.be>
2025-05-21 17:21:46 +0200

    added crecase and screenshots in main folder

---
+++ Crecase.sql
@@ -0,0 +1,13 @@
+﻿IF OBJECT_ID('Users', 'U') IS NOT NULL
+    DROP TABLE Users;
+
+CREATE TABLE Users (
+    ID INT PRIMARY KEY IDENTITY(1,1),
+    Username NVARCHAR(100) 
+        CONSTRAINT CK_Users_Username CHECK (LEN(Username) > 0) NOT NULL,
+    [Password] NVARCHAR(255) 
+        CONSTRAINT CK_Users_Password CHECK (LEN([Password]) > 0) NOT NULL,
+    Email NVARCHAR(255) NOT NULL
+        CONSTRAINT CK_Users_Email_Format CHECK (Email LIKE '%@%.%'),
+    CONSTRAINT UQ_Users_Email UNIQUE (Email)
+);
